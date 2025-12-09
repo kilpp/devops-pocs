49d2288bbdf5ca4e505a90146d8e692aedfaa0f2
gk <guikilpp@gmail.com>
2025-12-09 12:16:17 -0300

    Clean up classifiers in setup.py
    Removed unnecessary classifiers from setup.py.

--- kafka-poc/setup.py
+++ kafka-poc/setup.py
@@ -13,17 +13,6 @@ setup(
     url="https://github.com/kilpp/devops-pocs",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
     python_requires=">=3.8",
     install_requires=[
         "kafka-python-ng==2.2.2",
